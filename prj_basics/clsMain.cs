fa9a04c06ca74764b47bae05e2871e3836dfd3d0
DEADPOOL\James <jamesronaldbanez@gmail.com>
2021-01-18 14:52:52 +0800

    OOP Concepts

--- prj_basics/clsMain.cs
+++ prj_basics/clsMain.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using prj_basics_type_samples;
+
 
 namespace prj_basics
 {
-    class Program
+    class clsMain
     {
         public static int Main(string[] args)
         {
@@ -72,59 +74,7 @@ namespace prj_basics
     }
 
 
-    class ProgramType
-    {
 
-        public void showVariableType(int a, byte b, long c)
-        {
-            System.Console.WriteLine("a is {0}, To String returns {1}, Type is {2}", a, a.ToString(), a.GetType());
-            System.Console.WriteLine("b is {0}, To String returns {1}, Type is {2}", b, b.ToString(), b.GetType());
-            System.Console.WriteLine("c is {0}, To String returns {1}, Type is {2}", c, c.ToString(), c.GetType());
-        }
-
-        public void showResultType()
-        {
-            double d;
-            d = Math.Pow(2, 100);
-            Console.WriteLine("d is {0}, Type is {1}", d, d.GetType());
-            bool b;
-            b = true;
-            Console.WriteLine("b is {0}, Type is {1}", b, b.GetType());
-        }
-    }
-
-
-    struct SampleStruct
-    {
-        int m_int;
-        public SampleStruct(int value)
-        {
-            m_int = value;
-        }
-
-        public void Super()
-        {
-            Console.WriteLine("SampleStruct type current value is {0}", m_int);
-        }
-    }
-
-    enum SampleEnum
-    {
-        Item1,
-        Item2,
-        Item3,
-        Item4
-    }
-
-    class SampleReference
-    {
-        public int initVal;
-
-        public SampleReference(int value)
-        {
-            initVal = value;
-        }
-    }
 
    
 
--- prj_basics/clssOopConcepts.cs
+++ prj_basics/clssOopConcepts.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace prj_basics
+namespace prj_basics_oop_concepts
 {
-    class clsInheritance
+    class SampleClass
     {
+
     }
 }
